afa6d3c1764d066506a485e9d81e75a7a442ec83
Admin <Admin@DESKTOP-SBLFK3K>
2024-05-14 12:56:30 +0530

    save btn is added

--- src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
+++ src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
@@ -6,7 +6,11 @@ import org.openqa.selenium.support.FindBy;
 public class ProductPage {
 @FindBy(xpath = "//img[@alt='Create Product...']")
 private WebElement createNewProductBtn;
-
+@FindBy(xpath = "(//input[@class='crmbutton small save'])[1]")
+private WebElement saveBtn;
+public WebElement getSaveBtn() {
+	return saveBtn;
+}
 public WebElement getCreateNewProductBtn() {
 	return createNewProductBtn;
 }
