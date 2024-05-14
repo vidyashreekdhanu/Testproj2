ec103011a70e585aaac7a0d73278f0006967fcf4
Admin <Admin@DESKTOP-SBLFK3K>
2024-05-14 12:49:08 +0530

    Product edit

--- src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
+++ src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
@@ -6,6 +6,12 @@ import org.openqa.selenium.support.FindBy;
 public class ProductPage {
 @FindBy(xpath = "//img[@alt='Create Product...']")
 private WebElement createNewProductBtn;
+@FindBy(xpath = "//input[@name='productname']")
+private WebElement productEdit;
+
+public WebElement getProductEdit() {
+	return productEdit;
+}
 
 public WebElement getCreateNewProductBtn() {
 	return createNewProductBtn;
